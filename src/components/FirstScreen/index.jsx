2d7815795b249315d1ca137e279623ac23fc0664
Semyonenko Ivan <alienba6y@gmail.com>
2022-10-27 14:17:34 +0300

    eyes

--- src/components/FirstScreen/index.jsx
+++ src/components/FirstScreen/index.jsx
@@ -2,15 +2,30 @@ import React from "react";
 import "./style.css";
 
 const FirstScreen = () => {
+
+    document.addEventListener("mousemove", (evt) => {
+        const eyes = document.querySelectorAll(".first-screen__decorate-orbit");
+        [].forEach.call(eyes, function (eye) {
+            let mouseX = eye.getBoundingClientRect().right;
+            let mouseY = eye.getBoundingClientRect().top;
+            if (eye.classList.contains("eye-left")) {
+                mouseX = eye.getBoundingClientRect().left;
+            }
+            let radianDegrees = Math.atan2(evt.pageX - mouseX, evt.pageY - mouseY);
+            let rotationDegrees = radianDegrees * (180 / Math.PI) * -1 + 180;
+            eye.style.transform = `rotate(${rotationDegrees}deg)`;
+        })
+    });
+
     return (
         <section className="first-screen">
-            <h1 className="first-screen__title">frontend developer</h1>
             <div className="first-screen__wrapper">
+                <h1 className="first-screen__title">frontend developer</h1>
                 <p className="first-screen__text">hi, i’m alienba6y, a frontend developer based in moscow. create frontend since 2020. really like react!</p>
                 <code className="first-screen__code first-screen__code--animate">// import React from 'react';</code>
                 <a className="first-screen__link" href="#portfolio">my projects</a>
                 <div className="first-screen__decorate">
-                    <div className="first-screen__decorate-orbit">
+                    <div className="first-screen__decorate-orbit eye-left">
                         <div className="first-screen__decorate-cornea">
                             <div className="first-screen__decorate-pupil"></div>
                         </div>
@@ -20,7 +35,7 @@ const FirstScreen = () => {
                             <div className="first-screen__decorate-pupil"></div>
                         </div>
                     </div>
-                    <div className="first-screen__decorate-orbit">
+                    <div className="first-screen__decorate-orbit eye-left">
                         <div className="first-screen__decorate-cornea">
                             <div className="first-screen__decorate-pupil"></div>
                         </div>
